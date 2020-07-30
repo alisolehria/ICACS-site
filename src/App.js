de41a037b9b46275c56be0757d6adeb55eab1bf0
Ali <alimuzaffar@live.com>
2020-07-30 23:22:12 +0400

    Added call for papers content

--- src/App.js
+++ src/App.js
@@ -68,7 +68,7 @@ class Navbar extends React.Component {
             <ul class="navbar-nav ml-auto">
             
                 <li class="nav-item">
-                    <a class="nav-link scroll-link" href="#call">Call for Papers</a>
+                    <a class="nav-link " href="#call">Call for Papers</a>
                 </li>
                  <li class="nav-item">
                     <a class="nav-link scroll-link" href="#prevConf">Previous Conferences</a>
@@ -102,11 +102,14 @@ class TopContent extends React.Component {
  
         <div class="row">
             <div class="col-md-8 offset-md-2 text">
-                <h1 class="wow fadeInLeftBig">Conference Name 2020</h1>
+                <h1 class="wow fadeInLeftBig"> International Conference on Applied CyberSecurity (ACS) 2021</h1>
                 <div class="description wow fadeInLeftBig">
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sed ex id augue bibendum faucibus. Vestibulum felis nibh, varius ut ultricies sed, auctor eget dui. Vivamus justo ex, imperdiet quis dui id, commodo pharetra purus. Aenean posuere id tellus vel commodo. Vivamus at nulla quam. Donec turpis mauris, gravida vel erat eu, blandit dictum lacus. In dictum nulla nec lorem tempor, non porta lacus vehicula.
-                    </p>
+The International Conference on Applied CyberSecurity (ACS) aims to bring together researchers, practitioners, developers and students in the field of cybersecurity to exchange novelties, experience and ideas. </p>
+
+<p>We invite original unpublished papers from academics, government and industry practitioners as well as research students on their innovations in both theory and practice of cybersecurity. Papers could be on original designs and concepts, techniques and mechanisms, as well as field applications. </p>
+
+<p>“Submitted papers must not substantially overlap with papers that have been published or that are simultaneously submitted to a journal or a conference/workshop with proceedings.”                     </p>
                 </div>
          
             
@@ -117,7 +120,7 @@ class TopContent extends React.Component {
  <h1>Time to Conference</h1>
 </div>
            <div id = "countdown">
-   <DateCountdown dateTo='January 01, 2021 00:00:00 GMT+03:00' />
+   <DateCountdown dateTo='December 01, 2021 00:00:00 GMT+03:00' />
 
    </div>
    </div>
@@ -132,24 +135,113 @@ class TopContent extends React.Component {
 class CallForPapers extends React.Component {
   render(){
     return (
-         <ScrollableAnchor id={'call'}>
+         
          <section id="dark-bg">
       <div class = "container">
 <div id = "call">
 
       <h2>Call for Papers</h2>
-
+    
       <div class = "row">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget sem in nunc tempor iaculis sit amet ullamcorper neque. Sed lectus ex, vulputate vel ultricies vitae, gravida vel quam. Nunc aliquet fermentum sodales. Quisque mollis sit amet eros a euismod. Praesent non turpis volutpat, elementum quam ac, lobortis turpis. Nam nec ultrices turpis, volutpat sodales nibh. Nam tristique et libero quis varius. Pellentesque pulvinar enim in dolor venenatis, sit amet iaculis tellus malesuada. Phasellus eget pellentesque enim, at iaculis libero. Vestibulum semper, enim et semper tempus, turpis eros maximus sapien, sit amet semper augue nunc quis nisl.
-
-        Ut tristique diam hendrerit turpis ullamcorper, quis sagittis urna tempus. Duis egestas libero a ex fermentum, vitae fermentum libero interdum. Sed tempor libero libero. Suspendisse ut volutpat eros, facilisis dapibus enim. Integer varius, velit eget dignissim faucibus, dui orci accumsan elit, id tincidunt enim purus et mi. Vivamus pellentesque egestas tortor et varius. Integer vehicula risus quis odio aliquet tincidunt. Fusce euismod eros purus, ac bibendum dui tempor ac. In id nisi lorem.
-
-        Vivamus elementum neque et mi tristique, at porttitor turpis mattis. Proin id urna magna. Curabitur egestas dolor eget faucibus suscipit. Fusce sit amet blandit ante. In a metus at nisi dictum euismod quis eget magna. Etiam eu ex justo. Duis a nulla et nibh auctor commodo. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;</p>
-      </div>
-</div> 
+  <h3>Conference Topics</h3>
+  </div>
+  <div class ="row">
+        <p>Topics of interests are, but not limited to: </p>
+        <ul>
+            <li>Machine learning applications to security and data-driven security</li>
+            <li>AI security </li>
+            <li>Network and wireless security </li>
+            <li>Malware and unwanted software</li>
+            <li>Digital forensics & biometrics (multimedia security, mobile, watermarking & data hiding, etc.)</li>
+            <li>Information Security Management (governance, risk management, security policies, compliance, operations, incident management, usability & human factors, etc.) </li>
+            <li>Access management</li>
+            <li>Authentication and authorization</li>
+            <li>Intrusion and anomaly detection and prevention </li>
+            <li>Threat Modelling, Vulnerability Assessment and Security Assessment</li>
+            <li>Web security </li>
+            <li>Anonymity & Privacy</li>
+            <li>System and OS security</li>
+            <li>Cryptography applications </li>
+            <li>5G security</li>
+            <li>Social network security</li>
+            <li>Infrastructure security</li>
+            <li>Source code and binaries security</li>
+            <li>Secure computer architectures</li>
+            <li>IT security laws and policies</li>
+            <li>Denial-of-service attacks and countermeasures </li>
+            <li>Anonymity and privacy</li>
+            <li>Security awareness, education and training</li>
+            <li>Application security </li>
+            <li>Blockchains and distributed ledger security </li>
+            <li>Cloud security </li>
+            <li>Cyber physical systems security </li>
+            <li>Distributed systems security </li>
+            <li>Embedded systems security </li>
+            <li>Mobile security and privacy </li>
+            <li>Internet of Things Security</li>
+            <li>Security and privacy policies and metrics</li>
+            <li>Security architectures</li>
+      </ul>
 </div>
+<div class = "row">
+  <h3>Conference Tracks</h3>
+   </div>
+   <div class = "row">
+     <p>The above topics are organized into tracks as follows:</p>
+     <ul>
+     <li><h4>Industry 4.0 Security & AI</h4>
+     Machine learning applications to security and data-driven security, AI security, 5G security, Internet of Things Security, Cloud security, Operational technology security, Cyber physical systems security </li>
+     <li><h4>Ethical hacking and Digital forensics</h4>Malware and unwanted software, Source code and binaries security, Vulnerability Assessment and Security Assessment, multimedia security, mobile, watermarking & data hiding, denial-of-service attacks and countermeasures</li>
+     <li><h4>Information Security Management</h4>Governance, risk management, security policies, compliance, operations, incident management, usability & human factors, Security and privacy policies and metrics, Threat Modelling, IT security laws and policies, security awareness, education and training, access management</li>
+     <li><h4>Secure Infrastructure</h4>Network and wireless security, web security, mobile security & privacy, distributed systems security, anonymity and privacy, intrusion and anomaly detection and prevention, OS security, infrastructure security, authentication and authorization, biometrics, secure computer architectures, blockchains and distributed ledger security, social network security, application security, cryptography applications, security architectures, embedded systems security</li>
+     </ul>
+    </div>
+      <div class = "row">
+  <h3>Important Deadlines</h3> 
+   </div>
+<div class = "row">
+  
+     <ul>
+     <li>Technical Paper/Panel or Tutorial Proposal Submission Due Date: June 5, 2021 at midnight GMT. </li>
+     <li>Acceptance Notification: August 10, 2021 </li>
+     <li>Camera-ready Submission: September 1, 2021 </li>
+     <li>Early Bird Registration Deadline: September 1, 2021 </li>
+     <li>Conference Date: November 2021 </li>
+     </ul>
+</div>
+<div class = "row">
+  <h3>Paper Preparation</h3> 
+   </div>
+<div class = "row">
+(http://www.wikicfp.com/cfp/servlet/event.showcfp?eventid=106249&copyownerid=152894) 
+
+“The technical tracks shall accept regular papers of 6 pages maximum (2 additional pages allowed but at an extra charge in the final submission), 10pt font double column in IEEE format. Authors must present novel perspectives within the general scope of the conference.” 
+“All papers must strictly follow the IEEE Xplore paper template from the following link:  
+https://www.ieee.org/conferences/publishing/templates.html  
+Authors Kit: https://ieeeauthorcenter.ieee.org/  
+” 
+
+
+</div>
+
+<div class = "row">
+  <h3>Paper Submission</h3> 
+   </div>
+<div class = "row">
+<p>We use EDAS/EasyChair for submission.   
+“Authors need to:  </p>
+
+<ol>
+  <li>Create an account (if not already registered) with EDAS at https://confName.edas.info/ </li> 
+  <li>Submission link: https://edas.info/newPaper.php?c=27554</li>
+</ol>
+</div>
+</div>
+   
+</div> 
+
 </section>
-            </ScrollableAnchor>   
+            
       );
   }
 }
