fbadcbfebc0216f9c572adaba5c578fa4e9eebab
alisolehria <alimuzaffar@live.com>
2020-08-03 14:03:07 +0400

    Added links

--- src/App.js
+++ src/App.js
@@ -213,12 +213,11 @@ class CallForPapers extends React.Component {
   <h3>Paper Preparation</h3> 
    </div>
 <div class = "row">
-(http://www.wikicfp.com/cfp/servlet/event.showcfp?eventid=106249&copyownerid=152894) 
+(<a href= "http://www.wikicfp.com/cfp/servlet/event.showcfp?eventid=106249&copyownerid=152894" target="_blank">http://www.wikicfp.com/cfp/servlet/event.showcfp?eventid=106249&copyownerid=152894)</a>
 
 “The technical tracks shall accept regular papers of 6 pages maximum (2 additional pages allowed but at an extra charge in the final submission), 10pt font double column in IEEE format. Authors must present novel perspectives within the general scope of the conference.” 
 “All papers must strictly follow the IEEE Xplore paper template from the following link:  
-https://www.ieee.org/conferences/publishing/templates.html  
-Authors Kit: https://ieeeauthorcenter.ieee.org/  
+<a href = "https://www.ieee.org/conferences/publishing/templates.html" target = "_blank">https://www.ieee.org/conferences/publishing/templates.html</a> Authors Kit:  <a href = "https://ieeeauthorcenter.ieee.org/" target = "_blank">https://ieeeauthorcenter.ieee.org/</a>  
 ” 
 
 
@@ -232,8 +231,8 @@ Authors Kit: https://ieeeauthorcenter.ieee.org/
 “Authors need to:  </p>
 
 <ol>
-  <li>Create an account (if not already registered) with EDAS at https://confName.edas.info/ </li> 
-  <li>Submission link: https://edas.info/newPaper.php?c=27554</li>
+  <li>Create an account (if not already registered) with EDAS at <a href = "https://confName.edas.info/" target = "_blank">https://confName.edas.info/</a> </li> 
+  <li>Submission link: <a href = "https://edas.info/newPaper.php?c=27554" target = "_blank"> https://edas.info/newPaper.php?c=27554 </a></li>
 </ol>
 </div>
 </div>
