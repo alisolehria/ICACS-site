2429122845ffc457b8146ea0fb52c8c3b3213805
alisolehria <alimuzaffar@live.com>
2021-01-27 17:55:01 +0400

    Added conference date

--- src/App.js
+++ src/App.js
@@ -140,6 +140,7 @@ class TopContent extends React.Component {
         <div class="row">
             <div class="col-md-8 offset-md-2 text">
                 <h1 class="wow fadeInLeftBig"> International Conference on Applied CyberSecurity (ACS) 2021</h1>
+                <h3 class="conference_date"> 13-14 November 2021 | Dubai, UAE</h3>
                 <div class="description wow fadeInLeftBig">
                     <p>
 The International Conference on Applied CyberSecurity (ACS) aims to bring together researchers, practitioners, developers and students in the field of cybersecurity to exchange novelties, experience and ideas. </p>
@@ -157,7 +158,7 @@ The International Conference on Applied CyberSecurity (ACS) aims to bring togeth
  <h1>Time to Conference</h1>
 </div>
            <div id = "countdown">
-   <DateCountdown dateTo='December 01, 2021 00:00:00 GMT+03:00' />
+   <DateCountdown dateTo='November 13, 2021 00:00:00 GMT+03:00' />
 
    </div>
    </div>
@@ -318,6 +319,7 @@ class ImportantDates extends React.Component {
 <PageCarousel title="Important Dates"/>
 <div class="container">
 <div id = "call" class = "callPapers">
+<h2> Conference: 13-14 November 2021 </h2>
 <div class="row">
 
 <div class="col">
