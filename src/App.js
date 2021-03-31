fdda580bf7e04289db8fb9d8ff91eb6990fdd7d0
alisolehria <alimuzaffar@live.com>
2021-03-31 18:57:20 +0400

    Removed some html

--- src/App.js
+++ src/App.js
@@ -273,32 +273,7 @@ class CallForPapers extends React.Component {
      <li>Conference Date: November 2021 </li>
      </ul>
 </div>
-<div class = "row">
-  <h3>Paper Preparation</h3> 
-   </div>
-<div class = "row">
-(<a href= "http://www.wikicfp.com/cfp/servlet/event.showcfp?eventid=106249&copyownerid=152894" target="_blank">http://www.wikicfp.com/cfp/servlet/event.showcfp?eventid=106249&copyownerid=152894)</a>
 
-“The technical tracks shall accept regular papers of 6 pages maximum (2 additional pages allowed but at an extra charge in the final submission), 10pt font double column in IEEE format. Authors must present novel perspectives within the general scope of the conference.” 
-“All papers must strictly follow the IEEE Xplore paper template from the following link:  
-<a href = "https://www.ieee.org/conferences/publishing/templates.html" target = "_blank">https://www.ieee.org/conferences/publishing/templates.html</a> Authors Kit:  <a href = "https://ieeeauthorcenter.ieee.org/" target = "_blank">https://ieeeauthorcenter.ieee.org/</a>  
-” 
-
-
-</div>
-
-<div class = "row">
-  <h3>Paper Submission</h3> 
-   </div>
-<div class = "row">
-<p>We use EDAS/EasyChair for submission.   
-“Authors need to:  </p>
-
-<ol>
-  <li>Create an account (if not already registered) with EDAS at <a href = "https://confName.edas.info/" target = "_blank">https://confName.edas.info/</a> </li> 
-  <li>Submission link: <a href = "https://edas.info/newPaper.php?c=27554" target = "_blank"> https://edas.info/newPaper.php?c=27554 </a></li>
-</ol>
-</div>
 </div>
    
 </div> 
