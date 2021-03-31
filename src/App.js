ba33ceaf316b292dc6a257f14691c9fd9f45ed78
alisolehria <alimuzaffar@live.com>
2021-03-31 19:57:02 +0400

    Added html and removed important deadlines from call for papers

--- src/App.js
+++ src/App.js
@@ -260,20 +260,32 @@ class CallForPapers extends React.Component {
      Network and wireless security, smart cities security, web security, mobile security & privacy, distributed systems security, anonymity and privacy, intrusion and anomaly detection and prevention, OS security, infrastructure security, authentication and authorization, biometrics, secure computer architectures, blockchains and distributed ledger security, social network security, application security, cryptography applications, security architectures, embedded systems security</li>
      </ul>
     </div>
-      <div class = "row">
-  <h3>Important Deadlines</h3> 
+<div class = "row">
+  <h3>Paper Preparation</h3> 
    </div>
 <div class = "row">
-  
-     <ul>
-     <li>Technical Paper/Panel or Tutorial Proposal Submission Due Date: June 5, 2021 at midnight GMT. </li>
-     <li>Acceptance Notification: August 10, 2021 </li>
-     <li>Camera-ready Submission: September 1, 2021 </li>
-     <li>Early Bird Registration Deadline: September 1, 2021 </li>
-     <li>Conference Date: November 2021 </li>
-     </ul>
+(<a href= "http://www.wikicfp.com/cfp/servlet/event.showcfp?eventid=106249&copyownerid=152894" target="_blank">http://www.wikicfp.com/cfp/servlet/event.showcfp?eventid=106249&copyownerid=152894)</a>
+
+“The technical tracks shall accept regular papers of 6 pages maximum (2 additional pages allowed but at an extra charge in the final submission), 10pt font double column in IEEE format. Authors must present novel perspectives within the general scope of the conference.” 
+“All papers must strictly follow the IEEE Xplore paper template from the following link:  
+<a href = "https://www.ieee.org/conferences/publishing/templates.html" target = "_blank">https://www.ieee.org/conferences/publishing/templates.html</a> Authors Kit:  <a href = "https://ieeeauthorcenter.ieee.org/" target = "_blank">https://ieeeauthorcenter.ieee.org/</a>  
+” 
+
+
 </div>
 
+<div class = "row">
+  <h3>Paper Submission</h3> 
+   </div>
+<div class = "row">
+<p>We use EDAS/EasyChair for submission.   
+“Authors need to:  </p>
+
+<ol>
+  <li>Create an account (if not already registered) with EDAS at <a href = "https://confName.edas.info/" target = "_blank">https://confName.edas.info/</a> </li> 
+  <li>Submission link: <a href = "https://edas.info/newPaper.php?c=27554" target = "_blank"> https://edas.info/newPaper.php?c=27554 </a></li>
+</ol>
+</div>
 </div>
    
 </div> 
