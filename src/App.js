88a44b6c460daf151c59e03722befbb27f900d29
alisolehria <alimuzaffar@live.com>
2021-04-01 14:32:38 +0400

    Small changes

--- src/App.js
+++ src/App.js
@@ -267,8 +267,8 @@ class CallForPapers extends React.Component {
 (<a href= "http://www.wikicfp.com/cfp/servlet/event.showcfp?eventid=106249&copyownerid=152894" target="_blank">http://www.wikicfp.com/cfp/servlet/event.showcfp?eventid=106249&copyownerid=152894)</a>
 
 “The technical tracks shall accept regular papers of 6 pages maximum (2 additional pages allowed but at an extra charge in the final submission), 10pt font double column in IEEE format. Authors must present novel perspectives within the general scope of the conference.” 
-“All papers must strictly follow the IEEE Xplore paper template from the following link:  
-<a href = "https://www.ieee.org/conferences/publishing/templates.html" target = "_blank">https://www.ieee.org/conferences/publishing/templates.html</a> Authors Kit:  <a href = "https://ieeeauthorcenter.ieee.org/" target = "_blank">https://ieeeauthorcenter.ieee.org/</a>  
+“All papers must strictly follow the IEEE Xplore paper template from the following link:  &nbsp;
+<a href = "https://www.ieee.org/conferences/publishing/templates.html" target = "_blank">https://www.ieee.org/conferences/publishing/templates.html</a> &nbsp; Authors Kit: &nbsp; <a href = "https://ieeeauthorcenter.ieee.org/" target = "_blank">https://ieeeauthorcenter.ieee.org/</a>  
 ” 
 
 
