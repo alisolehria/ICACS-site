9fe2cf8c3f9137b27b5f0d82ae05e9da9a9b149b
alisolehria <alimuzaffar@live.com>
2021-07-28 23:46:52 +0400

    changed link text

--- src/Registration.js
+++ src/Registration.js
@@ -27,7 +27,7 @@ class Registration extends React.Component {
     <div class = "row">
     <h3>Registration Link</h3>
     </div>
-    <a href="https://www.eventbrite.com/e/the-international-conference-on-applied-cybersecurity-2021-acs21-tickets-164990451883" target="_blank">https://www.eventbrite.com/e/the-international-conference-on-applied-cybersecurity-2021-acs21-tickets-164990451883</a>
+    Registration link on <a href="https://www.eventbrite.com/e/the-international-conference-on-applied-cybersecurity-2021-acs21-tickets-164990451883" target="_blank">eventbrite.</a>
     </div>
     </div>
 
