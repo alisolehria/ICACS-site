da1a81bc17953a89cafde18ade03de0ce3fde9ae
alisolehria <alimuzaffar@live.com>
2022-07-20 13:47:56 +0400

    browser router added

--- src/Main.js
+++ src/Main.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Switch, Route, exact } from 'react-router-dom';
+import {BrowserRouter, Switch, Route, exact } from 'react-router-dom';
 
 import Home2023 from './2023/App2023.js';
 import ImportantDates from './2023/ImportantDates.js';
@@ -7,12 +7,14 @@ import Home2021 from './2021/App2021.js';
 
 const Main = () => {
   return (
-    <Switch> {/* The Switch decides which component to show based on the current URL.*/}
-      
-      {/* <Route exact path='/importantdates' component={ImportantDates}></Route> */}
-      <Route exact path='/prev/2021/*' component={Home2021}></Route>
-      <Route exact path='/*' component={Home2023}></Route>
-    </Switch>
+    <BrowserRouter>
+      <Switch> {/* The Switch decides which component to show based on the current URL.*/}
+        
+        {/* <Route exact path='/importantdates' component={ImportantDates}></Route> */}
+        <Route exact path='/prev/2021/*' component={Home2021}></Route>
+        <Route exact path='/*' component={Home2023}></Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
